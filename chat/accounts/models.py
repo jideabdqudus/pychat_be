33f30f246b10421201f9c36af4d52aa71a2efdd0
jideabdqudus <jideabdqudus@gmail.com>
2021-06-19 21:59:35 +0100

    +

--- chat/accounts/models.py
+++ chat/accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-from django.conf import settings
 
 
 class UserProfileManager(BaseUserManager):
--- chat/accounts/views.py
+++ chat/accounts/views.py
@@ -5,8 +5,6 @@ from .models import UserProfile
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from .permissions import UpdateOwnProfile
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 
 class RegisterViewSet(viewsets.ModelViewSet):
@@ -23,8 +21,8 @@ class UserLoginApiView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-class UserLogout(APIView):
-    def get(self, request):
-        # simply delete the token to force a login
-        request.user.auth_token.delete()
-        return Response(status=status.HTTP_200_OK)
+# class UserLogout(APIView):
+#     def get(self, request):
+#         # simply delete the token to force a login
+#         request.user.auth_token.delete()
+#         return Response(status=status.HTTP_200_OK)
